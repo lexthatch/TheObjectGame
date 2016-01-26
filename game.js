59bdf81c6ffc2c1fc40b39c7c7e22e28749b54b2
lexthatch <lexthatch@hotmail.com>
2016-01-25 20:54:31 -0700

    Health reporting

--- game.js
+++ game.js
@@ -4,4 +4,10 @@ function slap() {
     health = health - 1;
     alert(health);
 }
+document.getElementById(sHealth);
+function update(health)
+innerText=health
+
+document.getElementById("sHealth").textContent = health
+
 
