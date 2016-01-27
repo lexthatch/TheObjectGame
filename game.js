0605f2247fabf4bfa676ac2aaf8797198f6f5659
lexthatch <lexthatch@hotmail.com>
2016-01-27 16:45:56 -0700

    tweak

--- game.js
+++ game.js
@@ -4,9 +4,10 @@ function slap() {
     health = health - 1;
     alert(health);
 }
-document.getElementById(sHealth);
-function update(health)
+document.getElementById("sHealth");
+function update(health){
 innerText=health
+{
 
 document.getElementById("sHealth").textContent = health
 
